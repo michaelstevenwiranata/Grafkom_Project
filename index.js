404121138ac615cc8d4ea6521db8269309ff9c65
RCHD3388 <richardraferguy@gmail.com>
2024-05-10 22:25:33 +0700

    load models done

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import loadModels from './loadModels';
 
+// INITIAL SETUP
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const loader = new GLTFLoader();
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+// LIGHTING
+const ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.5); // soft white light
+ambientLight.position.set(20,20,20)
+scene.add(ambientLight);
 
-camera.position.z = 5;
+// LOAD MODELS
+loadModels(loader, scene);
 
+// ANIMATE
 function animate() {
 	requestAnimationFrame( animate );
-
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
 
 	renderer.render( scene, camera );
 }
--- loadModels.js
+++ loadModels.js
@@ -1,5 +1,22 @@
 async function loadModels(loader, scene){
-    
+    let ruangan, meja;
+    try {
+
+        // load model ruangan
+        const ruanganPromise = new Promise((resolve, reject) => {
+            loader.load('/public/ruangan.glb', function ( gltf ) {
+                ruangan = gltf.scene;
+                ruangan.scale.set(10,10,10)
+                scene.add(ruangan);
+                resolve();
+            }, undefined, reject);
+        });
+        await ruanganPromise;
+
+        await Promise.all([ruanganPromise]);
+    } catch (error) {   
+        console.log("Error loading models : " + error);
+    }
 }
 
 export default loadModels;
