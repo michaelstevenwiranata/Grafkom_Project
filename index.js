52d1fe9fbbeee38fb18b0a5fbf607813e47d438d
RCHD88 <richardraferguy@gmail.com>
2024-05-10 18:28:03 +0700

    jancok

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockCont
 const loader = new GLTFLoader();
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+// set camera 
