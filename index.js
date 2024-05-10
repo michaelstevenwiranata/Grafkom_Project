9ea606b35adec727ba038b64d03d19c9acd289cc
RCHD88 <richardraferguy@gmail.com>
2024-05-10 18:53:23 +0700

    test

--- index.js
+++
@@ -1,9 +0,0 @@
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls.js';
-
-const loader = new GLTFLoader();
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-// set camera 
